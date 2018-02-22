7510fecd0110caa4e0c9b29803d7fd046c55cea8
Ilya <chernikov.i123@gmail.com>
2018-02-23 00:02:51 +1000

    Replace fake map with google map

--- src/js/main.js
+++ src/js/main.js
@@ -442,10 +442,12 @@ $(document)
         sectionSelector: '.fullpage__section'
       });
 
-    $('.fullpage__anchor').click(function() {
-      var section = $(this).attr('data-href')
-      $.fn.fullpage.moveTo(section)
-    });
+    $('.fullpage__anchor')
+      .click(function() {
+        var section = $(this)
+          .attr('data-href');
+        $.fn.fullpage.moveTo(section);
+      });
 
     //////////
     // DEVELOPMENT HELPER
@@ -473,3 +475,110 @@ $(document)
     }
 
   });
+
+// Map init
+function initMap() {
+  let map,
+    marker;
+
+  const mapInitPos = {
+    lat: 40.757483,
+    lng: -73.971084
+  };
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: mapInitPos,
+    zoom: 17,
+    mapTypeControlOptions: {
+      mapTypeIds: []
+    }, // here´s the array of controls
+    disableDefaultUI: true, // a way to quickly hide all controls
+    mapTypeControl: true,
+    scaleControl: true,
+    zoomControl: true,
+    styles: [
+      {
+        'featureType': 'water',
+        'elementType': 'geometry',
+        'stylers': [{ 'color': '#e9e9e9' }, { 'lightness': 17 }]
+      }, {
+        'featureType': 'landscape',
+        'elementType': 'geometry',
+        'stylers': [{ 'color': '#f5f5f5' }, { 'lightness': 20 }]
+      }, {
+        'featureType': 'road.highway',
+        'elementType': 'geometry.fill',
+        'stylers': [{ 'color': '#ffffff' }, { 'lightness': 17 }]
+      }, {
+        'featureType': 'road.highway',
+        'elementType': 'geometry.stroke',
+        'stylers': [{ 'color': '#ffffff' }, { 'lightness': 29 }, { 'weight': 0.2 }]
+      }, {
+        'featureType': 'road.arterial',
+        'elementType': 'geometry',
+        'stylers': [{ 'color': '#ffffff' }, { 'lightness': 18 }]
+      }, {
+        'featureType': 'road.local',
+        'elementType': 'geometry',
+        'stylers': [{ 'color': '#ffffff' }, { 'lightness': 16 }]
+      }, {
+        'featureType': 'poi',
+        'elementType': 'geometry',
+        'stylers': [{ 'color': '#f5f5f5' }, { 'lightness': 21 }]
+      }, {
+        'featureType': 'poi.park',
+        'elementType': 'geometry',
+        'stylers': [{ 'color': '#dedede' }, { 'lightness': 21 }]
+      }, {
+        'elementType': 'labels.text.stroke',
+        'stylers': [{ 'visibility': 'on' }, { 'color': '#ffffff' }, { 'lightness': 16 }]
+      }, {
+        'elementType': 'labels.text.fill',
+        'stylers': [{ 'saturation': 36 }, { 'color': '#333333' }, { 'lightness': 40 }]
+      }, {
+        'elementType': 'labels.icon',
+        'stylers': [{ 'visibility': 'off' }]
+      }, {
+        'featureType': 'transit',
+        'elementType': 'geometry',
+        'stylers': [{ 'color': '#f2f2f2' }, { 'lightness': 19 }]
+      }, {
+        'featureType': 'administrative',
+        'elementType': 'geometry.fill',
+        'stylers': [{ 'color': '#fefefe' }, { 'lightness': 20 }]
+      }, {
+        'featureType': 'administrative',
+        'elementType': 'geometry.stroke',
+        'stylers': [{ 'color': '#fefefe' }, { 'lightness': 17 }, { 'weight': 1.2 }]
+      }
+    ]
+  });
+
+
+  const infowindow = new google.maps.InfoWindow({
+    content: `<div class="g-marker">
+               <p>???</p>
+              </div>`
+  });
+
+  marker = new google.maps.Marker({
+    position: mapInitPos,
+    icon: {
+      url: '../img/mapMarker.png',
+      scaledSize: new google.maps.Size(160, 120),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(80, 115)
+    },
+    map: map,
+    title: 'Tokio',
+    animation: google.maps.Animation.DROP
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+  google.maps.event.addListener(map, 'click', function() {
+    infowindow.close();
+  });
+}
